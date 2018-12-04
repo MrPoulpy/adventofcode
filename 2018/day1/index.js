c131d94e9138077243cebb61084d393b31d76186
Denis Labaste <denis@labaste.fr>
2018-12-04 04:12:45 +0100

    Part 2 finished.

--- 2018/day1/index.js
+++ 2018/day1/index.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
-const input = fs.readFileSync('input.txt').toString().split('\n');
+const inputMapped = fs.readFileSync('input.txt').toString().split('\n').map(val => +val);
 
-let answer = input.map(val => +val).reduce((a, b) => a + b, 0);
+let answer = inputMapped.reduce((a, b) => a + b, 0);
 
-console.log(answer);
+console.log(answer); //433
+
+const frequencies = new Set();
+let frequency = 0;
+let cnt = 0;
+
+while (!frequencies.has(frequency)) {
+    frequencies.add(frequency);
+
+    if (cnt === inputMapped.length) {
+        cnt = 0;
+    }
+    frequency += inputMapped[cnt];
+    cnt++;
+}
+
+console.log(frequency); //256
