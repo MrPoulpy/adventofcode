74cd64e9a3e19d29c72639860de54bc264f71e4a
Denis LABASTE <dlabaste@feelity.fr>
2019-12-06 09:23:06 +0100

    init day 6

--- 2019/day5/index.js
+++ 2019/day5/index.js
@@ -5,34 +5,29 @@ const convert = (arr, mode = '0', pos) => {
    return mode === '0' ? arr[pos] : pos;
 };
 
-const parse = raw => {
-    const opcode = raw.toString().slice(-2);
-    const mode = raw.toString().slice(0, -2).split('');
-    return [mode, +opcode];
-};
-
 const calcDiagnostic = (instructions, input) => {
     for (let i = 0; i < instructions.length;) {
-        const [rawOpcode, val1, val2, out] = instructions.slice(i, i + 4);
-        const [modes, opcode] = parse(rawOpcode);
+        const [raw, val1, val2, out] = instructions.slice(i, i + 4);
+        const opcode = parseInt(raw.toString().slice(-2));
+        const mode = raw.toString().slice(0, -2).split('');
 
         switch (opcode) {
             case 1:
-                instructions[out] = convert(instructions, modes.pop(), val1) + convert(instructions, modes.pop(), val2); i += 4; break;
+                instructions[out] = convert(instructions, mode.pop(), val1) + convert(instructions, mode.pop(), val2); i += 4; break;
             case 2:
-                instructions[out] = convert(instructions, modes.pop(), val1) * convert(instructions, modes.pop(), val2); i += 4; break;
+                instructions[out] = convert(instructions, mode.pop(), val1) * convert(instructions, mode.pop(), val2); i += 4; break;
             case 3:
                 instructions[val1] = input; i += 2; break;
             case 4:
                 console.log(instructions[val1]); i += 2; break;
             case 5:
-                i = convert(instructions, modes.pop(), val1) !== 0 ? convert(instructions, modes.pop(), val2) : i + 3; break;
+                i = convert(instructions, mode.pop(), val1) !== 0 ? convert(instructions, mode.pop(), val2) : i + 3; break;
             case 6:
-                i = convert(instructions, modes.pop(), val1) === 0 ? convert(instructions, modes.pop(), val2) : i + 3; break;
+                i = convert(instructions, mode.pop(), val1) === 0 ? convert(instructions, mode.pop(), val2) : i + 3; break;
             case 7:
-                instructions[out] = convert(instructions, modes.pop(), val1) < convert(instructions, modes.pop(), val2) ? 1 : 0; i += 4; break;
+                instructions[out] = convert(instructions, mode.pop(), val1) < convert(instructions, mode.pop(), val2) ? 1 : 0; i += 4; break;
             case 8:
-                instructions[out] = convert(instructions, modes.pop(), val1) === convert(instructions, modes.pop(), val2) ? 1 : 0; i += 4; break;
+                instructions[out] = convert(instructions, mode.pop(), val1) === convert(instructions, mode.pop(), val2) ? 1 : 0; i += 4; break;
             case 99:
                 return;
         }
